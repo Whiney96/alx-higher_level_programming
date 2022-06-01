405e59297a120d0c0484f058dd232e12644c2b4a
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 08:23:33 -0700

    used f-strings

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for i in range(0, 10):
     for c in range(0, 10):
         if i == c:
             continue
+        elif int(f"{i}{c}") == int(f"{c}{i}"):
+            continue
         else:
             print("{0}{1}".format(i, c), end=(", "))
