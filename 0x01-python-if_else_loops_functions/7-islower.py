3ee3f7fe9c84ad9f8dc480fb9851ee82f6f9a22a
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 10:17:07 -0700

    changed islower(c)

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def islower(c):
     islower = __import__('7-islower').islower
 
-    if islower(c) is True:
+    if c is True:
         print("islower(c) is {}".format("upper"))
     else:
         print("islower(c) is {}".format("lower"))
