8beb36b9130c0ee13d274b363c26b73df80256c2
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 14:41:08 -0700

    added another print()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(65, 91):
-        print("{0:c}".format(i))
+    for i in str:
+        if str == 65:
+            print("{0:c}".format(i))
+        elif str < 91:
+            print("{0:c}".format(i))
