ef21e1664085174e28f61457ae9ed2b00c8c9442
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 14:53:50 -0700

    added islower

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if str == 65:
-            print("{0:c}".format(i))
-        elif str < 91:
-            print("{0:c}".format(i))
+        if str.islower():
+            print("{0:C}".format(i))
