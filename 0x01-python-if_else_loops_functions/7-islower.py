f8e5907939f02314bae102f278d7a106db5181bc
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 12:42:20 -0700

    added for loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    islower = __import__('7-islower').islower
-
-    if c is True:
-        print(c, " is {}".format("upper"))
-    else:
-        print(c, " is {}".format("lower"))
+    for x in c:
+        if x.islower():
+            return True
+        else:
+            return False
