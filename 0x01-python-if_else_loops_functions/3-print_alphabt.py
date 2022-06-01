099b6dfc561f3be9286e9f81db60e7bf2a606dea
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 05:51:56 -0700

    changed ASCII value

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if c == 101:
+    if c == 113:
         continue
-        if c == 116:
-            continue
-    else:
-        print("{:c}".format(c), end="")
+    elif c == 101:
+        continue
+    print("{:c}".format(c), end="")
