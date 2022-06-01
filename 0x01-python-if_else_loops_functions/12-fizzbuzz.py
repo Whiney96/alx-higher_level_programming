b6c4b12bebff8bfa314359859cf752a10bcc7b8f
Whiney96 <Uziwinnie@gmail.com>
2022-06-01 16:30:26 -0700

    added end()

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz ")
+            print("{}".format(Fizz), end=(" "))
         elif i % 5 == 0:
-            print("Buzz ")
+            print("{}".formate(Buzz), end=(" "))
         elif (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz ")
+            print("FizzBuzz", end=(" "))
         else:
-            print("{} ".format(i))
+            print("{} ".format(i), end=(" "))
