a5678307536140d6435e410110b0c3c5ab489bcd
whiney96 <uziwinnie@gmail.com>
2022-06-02 16:24:26 -0700

    code changed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 def add(a, b):
-    """My addition function
-
-
-    Args:
-    a: first integer
-    b: second integer
-
-
-    Returns:
-        The return value. a + b
-    """
     a = 1
     b = 2
     print("{0} + {1} = {2}".format(a, b, add))
