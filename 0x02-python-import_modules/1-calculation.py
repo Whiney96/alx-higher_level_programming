d548b9afa29f2f3f164ad2d415deb1e661986f45
whiney96 <uziwinnie@gmail.com>
2022-06-02 17:31:14 -0700

    task 1 file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import calculator_1
+def add(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+def sub(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+
+def mul(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+
+def div(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, div(a, b)))
+
+    if __name__ == "__main__":
+        main()
