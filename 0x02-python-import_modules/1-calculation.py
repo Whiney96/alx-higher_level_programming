416b0ba6b5b8285991ec949f3110bfee22cd8ecc
whiney96 <uziwinnie@gmail.com>
2022-06-02 17:39:00 -0700

    corrected operators

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,17 +8,17 @@ def add(a, b):
 def sub(a, b):
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
 
 def mul(a, b):
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 
 def div(a, b):
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
     if __name__ == "__main__":
         import sys
