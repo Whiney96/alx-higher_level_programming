2f4263b4d3b0210642b93bee9d5fa640b989e7a8
whiney96 <uziwinnie@gmail.com>
2022-06-02 17:09:58 -0700

    code changed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
