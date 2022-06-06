12eca9cc3624504e4d206ce537aa1f15ff910ee9
whiney96 <uziwinnie@gmail.com>
2022-06-05 17:45:36 -0700

    removed squared brackets

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in range(0, len(my_list)):
+    for idx in range(0, len(my_list[])):
         if idx < 0:
             return None
-        elif idx > len(my_list[]):
+        elif idx > len(my_list):
             return None
         print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
