e00562de74e7963f3e6aa26c49d07ad76474bfbf
whiney96 <uziwinnie@gmail.com>
2022-06-05 17:42:47 -0700

    changed 'x' to 'idx'

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for x in range(0, len(my_list)):
+    for idx in range(0, len(my_list)):
         if idx < 0:
             return None
         elif idx > len(my_list[]):
