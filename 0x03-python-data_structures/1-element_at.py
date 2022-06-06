927df09d93c1fbcff952cece7d3ad5dad47ce92a
whiney96 <uziwinnie@gmail.com>
2022-06-05 17:40:22 -0700

    added a for loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list[]):
-        return None
-    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    for x in range(0, len(my_list)):
+        if idx < 0:
+            return None
+        elif idx > len(my_list[]):
+            return None
+        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
