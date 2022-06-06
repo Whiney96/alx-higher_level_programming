1dd9c7cb2826121287bec5e3a49e62f5eb37eed5
whiney96 <uziwinnie@gmail.com>
2022-06-05 17:37:08 -0700

    added square brackets

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > len(my_list[]):
         return None
     print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
