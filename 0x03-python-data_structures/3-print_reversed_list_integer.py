def1b0edad2655a61e850cb8cbef7b57c0c466ac
whiney96 <uziwinnie@gmail.com>
2022-06-05 18:29:18 -0700

    removed square brackets

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    for x in range(0, my_list[]):
+
+    for x in range(0, my_list):
         print("{:d}".format(my_list[x])
