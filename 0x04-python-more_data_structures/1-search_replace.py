5c98446474e1013013773636ef15f01659480fd5
whiney96 <uziwinnie@gmail.com>
2022-06-10 03:41:42 -0700

    removed parentheses

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [(new.append(replace) if x == search else new.append(x))for x in my_list]
+    new = [new.append(replace) if x == search else new.append(x) for x in my_list]
     return new
