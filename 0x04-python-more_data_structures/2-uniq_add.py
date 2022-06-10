a7e61b296a3ccd9130726dab6f1c402f88de230c
whiney96 <uziwinnie@gmail.com>
2022-06-10 04:15:46 -0700

    included whitespaces

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    from functools import reduce
-    result = reduce(lambda x,y: x+y, set(my_list))
+    import functools
+    result = reduce(lambda x, y: x + y, set(my_list))
     print("Result: {:d}".format(result))
