807ca92a2c0991253660e4f46cf1d3bad97e4a1e
whiney96 <uziwinnie@gmail.com>
2022-06-10 03:27:44 -0700

    adjusted strings

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for x in my_list:
-    return (list(lambda x, 'replace', 'search': 'replace' if x == 'search' else x))
+    return (list(lambda x, replace, search: replace if x == search else x))
