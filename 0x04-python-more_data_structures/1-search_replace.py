984fd9ca0c151d047c04e4556b01b5863ba1a2fc
whiney96 <uziwinnie@gmail.com>
2022-06-10 03:31:57 -0700

    included variables

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    a = 'search'
+    b = 'replace'
     for x in my_list:
-    return (list(lambda x, replace, search: replace if x == search else x))
+    return (list(lambda x, b, a: a if x == b else x))
