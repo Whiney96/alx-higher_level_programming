6bb5142f321742b5e7db6eaf3c342a77e9c87747
whiney96 <uziwinnie@gmail.com>
2022-06-10 03:25:23 -0700

    adjusted for

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (list(lambda x, 'replace', 'search': 'replace' if x == 'search' else x)for x in my_list)
+    for x in my_list:
+    return (list(lambda x, 'replace', 'search': 'replace' if x == 'search' else x))
