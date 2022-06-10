b68a896edabf6aafe586070764bd7db1cd83a439
whiney96 <uziwinnie@gmail.com>
2022-06-10 04:13:45 -0700

    imported reduce

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    set(my_list)
-    for x in my_list:
-        result = x + x
-        print("Result: {:d}".format(result))
+    from functools import reduce
+    result = reduce(lambda x,y: x+y, set(my_list))
+    print("Result: {:d}".format(result))
