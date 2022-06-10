3421275278221e38bdc715d78f8de90f9649d23d
whiney96 <uziwinnie@gmail.com>
2022-06-10 03:35:03 -0700

    changed for positon

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,4 @@
 def search_replace(my_list, search, replace):
     a = 'search'
     b = 'replace'
-    for x in my_list:
-    return (list(lambda x, b, a: a if x == b else x))
+    return (list(lambda x, b, a: a if x == b else x) for x in my_list)
